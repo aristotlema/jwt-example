d4ed7f3660e809b69cf27d4786f10bc8b7fe3570
aristotlema <aristotle.maule@hotmail.com>
2021-05-22 08:31:46 -0400

    DB Config, setup User model and migration

---
+++ server/migrations/20210522122152-create-user.js
@@ -0,0 +1,32 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, DataTypes) => {
+    await queryInterface.createTable('users', {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      name: DataTypes.STRING,
+      userName: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      passWord: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      created_at: {
+        allowNull: false,
+        type: DataTypes.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: DataTypes.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('users');
+  }
+};
--- server/server.js
+++ server/server.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
+const { sequelize } = require('./models');
 
-app.listen(4001, () => {
+app.listen(4001, async () => {
     console.log('Server is running on port 4000');
+    try {
+        await sequelize.authenticate();
+        console.log('Database Connected Succesfully');
+    } catch (err) {
+        console.error('Connection to Databse Failed', err);
+    }
 });
