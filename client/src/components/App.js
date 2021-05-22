cc573cd5456899f54b7f78aaf505a996b701d433
aristotlema <aristotle.maule@hotmail.com>
2021-05-22 08:12:22 -0400

    setup React router and create account form

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { Router, Route } from 'react-router-dom';
 import LoginForm from '../components/AuthForms/LoginForm';
+import CreatFrom from '../components/AuthForms/CreateForm';
 import './App.scss';
+import history from '../history';
 
 const App = () => {
     return (
         <main>
-            <LoginForm />
+            <Router history={history} >
+                <div>
+                    <Route path="/" exact component={LoginForm} />
+                    <Route path="/login/create" exact component={CreatFrom} />
+                </div>
+            </Router>
         </main>
     );
 };
--- client/src/components/AuthForms/CreateForm.js
+++ client/src/components/AuthForms/CreateForm.js
@@ -2,8 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 
-
-const LoginForm = () => {
+const CreateForm = () => {
     const { register, handleSubmit } = useForm();
 
     const onSubmit = (data) => {
@@ -12,13 +11,15 @@ const LoginForm = () => {
 
     return (
         <>
-            <h2>Login</h2>
+            <h2>Creat an Account</h2>
             <form onSubmit={handleSubmit(onSubmit)}>
                 <label htmlFor="userName">User Name</label>
                 <input {...register("userName")} />
-                <label htmlFor="passWord">Password</label>
+                <label htmlFor="passWord">password</label>
                 <input {...register("passWord")} type="password"/>
-                <Link to="/login/create">Create an account</Link>
+                <p>
+                    Already have an account? <Link to="/">Sign in</Link>
+                </p>
 
                 <input type="submit" />
             </form>
@@ -26,4 +27,4 @@ const LoginForm = () => {
     );
 };
 
-export default LoginForm;
+export default CreateForm
